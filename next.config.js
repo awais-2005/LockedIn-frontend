f9c3b4cf24bdc04ae93d19f411ccb858b536b8db
awais-2005 <rmawais852689@gmail.com>
2026-07-20 08:41:03 +0500

    reverted next.config.js

--- next.config.js
+++ next.config.js
@@ -9,20 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [{
-          key: "Cross-Origin-Opener-Policy",
-          value: "unsafe-none",
-        }],
-      },
-    ];
-  },
-  async rewrites() {
-    return [{ source: '/api/:path*', destination: 'https://lockedin-lj34.onrender.com/api/:path*' }];
-  }
 };
 
 module.exports = nextConfig;
